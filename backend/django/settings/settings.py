9d25cd99d8b8cbc5f95d6e9b13efaf8179fa0acc
SirDalbag <sir.dalbag@gmail.com>
2024-04-16 13:43:22 +0500

    adding django

---
+++ backend/django/settings/settings.py
@@ -0,0 +1,118 @@
+from pathlib import Path
+from dotenv import load_dotenv
+from socket import gethostname
+from os import getenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+ENV_DIR = BASE_DIR / ".env"
+
+load_dotenv(ENV_DIR)
+
+if not getenv("SECRET_KEY"):
+    raise Exception("ENV NOT FOUND!")
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+HOST_NAMES = getenv("HOST_NAMES", "").split(",")
+
+DEBUG = gethostname() in HOST_NAMES
+
+ALLOWED_HOSTS = getenv("ALLOWED_HOSTS", "").split(",")
+
+CORS_ALLOW_ALL_ORIGINS = True if getenv("CORS_ALLOW_ALL_ORIGINS") == "True" else False
+
+CORS_ALLOWED_ORIGINS = getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
+    "app",
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "settings.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "settings.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("DATABASE_NAME"),
+        "USER": getenv("DATABASE_USER"),
+        "PASSWORD": getenv("DATABASE_PASSWORD"),
+        "HOST": getenv("DATABASE_HOST"),
+        "PORT": getenv("DATABASE_PORT"),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = "static/"
+if not DEBUG:
+    STATIC_ROOT = Path(BASE_DIR / "static")
+STATICFILES_DIRS = [Path(BASE_DIR / "static")]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = Path(BASE_DIR / "static/media")
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
